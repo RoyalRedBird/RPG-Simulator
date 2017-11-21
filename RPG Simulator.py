4a585c717090fe0f39afa68ce8ca0f7fca0fc8bc
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2017-11-21 14:31:08 -0500

    Add files via upload

--- RPG Simulator.py
+++ RPG Simulator.py
@@ -8,6 +8,10 @@ Player_HP = 100
 
 Monster_HP = 75
 
+HP_Potions = 3
+
+Vigor_Potions = 3
+
 while Player_HP > 0 and Monster_HP > 0:
 
     Player_Action = input("What will you do?")
@@ -24,7 +28,55 @@ while Player_HP > 0 and Monster_HP > 0:
 
         print ("The monster has %s health left!" % Monster_HP)
 
-    print ("Brace for combat!")
+    if Player_Action == ("Block"):
+
+        Player_Block = random.uniform(4,8)
+
+        Player_Block = int(Player_Block)
+
+        print ("You brace yourself...")
+
+    if Player_Action == ("Use"):
+
+        Player_Item = input("What item wil you use?")
+
+        if Player_Item == ("HP Potion"):
+
+            if HP_Potions > 0:
+
+                print ("You drink a health potion.")
+
+                Potion_Recovery = random.uniform (20,30)
+
+                Potion_Recovery = int(Potion_Recovery)
+
+                Player_HP = Player_HP + Potion_Recovery
+
+                print ("You recover %s health." % Potion_Recovery)
+
+                Potions = Potions - 1
+
+                print ("You have %s potions left!" % Potions)
+
+                print ("You now have %s health left." % Player_HP)
+
+            else:
+
+                print ("You are all out of potions!")
+
+        if Player_Item == ("Vigor Potion"):
+
+            if Vigor_Potions > 0:
+
+                print ("You drink a vigor potion.")
+
+                
+        
+    if Monster_HP <= 0:
+
+        break
+
+    print ("Get Ready!")
 
     time.sleep(2)
 
@@ -34,12 +86,30 @@ while Player_HP > 0 and Monster_HP > 0:
 
     print ("The monster hit you for %s damage!" % Monster_Damage)
 
-    Player_HP = Player_HP - Monster_Damage
+    if Player_Action == ("Block"):
+
+        print ("However, you block for %s damage!" % Player_Block)
+
+        Player_HP = Player_HP - Monster_Damage + Player_Block
+
+    else:
+
+        Player_HP = Player_HP - Monster_Damage
 
     print ("You have %s health left!" % Player_HP)
+
+    if Player_HP <= 0:
+
+        break
 
     print ("Compose yourself.")
 
     time.sleep(3)
 
-    
+if Monster_HP <= 0:
+
+    print ("Huzzah! The foul beast is slain!")
+
+if Player_HP <= 0:
+
+    print ("Your journey has come to a painful, sombre end...")
